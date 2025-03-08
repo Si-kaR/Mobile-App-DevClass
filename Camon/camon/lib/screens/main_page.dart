1215c6cab422b4022c5b56c4e2432839e817ca1b
Si-kaR <robert.sika@ashesi.edu.gh>
2025-03-08 11:07:18 +0000

    second commit - mobile app accesses camera

---
+++ Camon/camon/lib/screens/main_page.dart
@@ -0,0 +1,63 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const MainPage({super.key, required this.cameras});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late CameraController cameraController;
+  late Future<void> cameraValue;
+
+  void startCamera(int camera) {
+    cameraController = CameraController(
+        widget.cameras[0], ResolutionPreset.high,
+        enableAudio: false);
+    cameraValue = cameraController.initialize();
+  }
+
+  @override
+  void initState() {
+    startCamera(0);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          FutureBuilder<void>(
+            future: cameraValue,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // return CameraPreview(cameraController);
+                return SizedBox(
+                  width: size.width,
+                  height: size.height,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: size.width,
+                      height: size.height,
+                      child: CameraPreview(cameraController),
+                    ),
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
