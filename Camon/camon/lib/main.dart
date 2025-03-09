18cebbaab7b9b39002a784f1a7b9aebb7522543e
Si-kaR <robert.sika@ashesi.edu.gh>
2025-03-09 00:53:05 +0000

    fourth commit - camera capture button added

--- Camon/camon/lib/main.dart
+++ Camon/camon/lib/main.dart
@@ -4,32 +4,32 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   final cameras = await availableCameras();
-  runApp(MyApp(
-    cameras: cameras,
-  ));
+  runApp(MyApp(cameras: cameras));
 }
 
 class MyApp extends StatelessWidget {
   final List<CameraDescription> cameras;
-  const MyApp({super.key, required this.cameras});
+
+  MyApp({required this.cameras});
+
+  // const MyApp({super.key, required this.cameras});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Camera Demo',
         theme: ThemeData(
           // This is the theme of your application.
-          //
+
           // TRY THIS: Try running your application with "flutter run". You'll see
           // the application has a purple toolbar. Then, without quitting the app,
           // try changing the seedColor in the colorScheme below to Colors.green
           // and then invoke "hot reload" (save your changes or press the "hot
           // reload" button in a Flutter-supported IDE, or press "r" if you used
           // the command line to start the app).
-          //
+
           // Notice that the counter didn't reset back to zero; the application
           // state is not lost during the reload. To reset the state, use hot
           // restart instead.
@@ -134,3 +134,43 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:camera/camera.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   final cameras = await availableCameras();
+//   runApp(MyApp(cameras: cameras));
+// }
+
+// class MyApp extends StatelessWidget {
+//   final List<CameraDescription> cameras;
+
+//   const MyApp({super.key, required this.cameras});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: CameraScreen(cameras: cameras),
+//     );
+//   }
+// }
+
+// class CameraScreen extends StatelessWidget {
+//   final List<CameraDescription> cameras;
+
+//   const CameraScreen({super.key, required this.cameras});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Camera Example'),
+//       ),
+//       body: Center(
+//         child: Text('Camera Screen'),
+//       ),
+//     );
+//   }
+// }
