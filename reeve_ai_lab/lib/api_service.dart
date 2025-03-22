92434648ff7c53b6487e8b3021d4485945d72b81
Si-kaR <robert.sika@ashesi.edu.gh>
2025-03-22 00:06:42 +0000

    Reeve Chatbot App - everything works except responses nto received

--- reeve_ai_lab/lib/api_service.dart
+++ reeve_ai_lab/lib/api_service.dart
@@ -6,6 +6,6 @@
 // Key2
 // const apikeyOpenReeveAi = "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";
 
-// Key3
-const apikeyOpenReeveAi =
-    "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";
+// Key3 - uncomment
+// const apikeyOpenReeveAi =
+//     "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";
