55f1870edbb83830ecb8802ac29a00a5a25c30e7
Si-kaR <robert.sika@ashesi.edu.gh>
2025-03-22 12:34:57 +0000

    ReeveAi Chatbot - Chat Not Text Back. Added Error Handling, created API-Service-Folder&Function - Added Demo Screenshot - Added Logo to Reeve Ai

--- reeve_ai_lab/lib/services/api_service.dart
+++ reeve_ai_lab/lib/services/api_service.dart
@@ -1,31 +1,34 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+const apikeyOpenReeveAi =
+    "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";
 
-class ApiService {
-  Future<String> fetchAIResponse(String message) async {
-    try {
-      final response = await http.post(
-        Uri.parse('https://api.openai.com/v1/chat/completions'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'message': message}),
-      );
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
 
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
+// class ApiService {
+//   Future<String> fetchAIResponse(String message) async {
+//     try {
+//       final response = await http.post(
+//         Uri.parse('https://api.openai.com/v1/chat/completions'),
+//         headers: {'Content-Type': 'application/json'},
+//         body: jsonEncode({'message': message}),
+//       );
 
-        if (data['message'] != null) {
-          return data['message'];
-        } else {
-          throw Exception("Invalid response structure");
-        }
-      } else {
-        throw Exception("Failed to load AI response (${response.statusCode})");
-      }
-    } catch (e) {
-      throw Exception("Oops! Something went wrong: $e");
-    }
-  }
-}
+//       if (response.statusCode == 200) {
+//         final data = jsonDecode(response.body);
+
+//         if (data['message'] != null) {
+//           return data['message'];
+//         } else {
+//           throw Exception("Invalid response structure");
+//         }
+//       } else {
+//         throw Exception("Failed to load AI response (${response.statusCode})");
+//       }
+//     } catch (e) {
+//       throw Exception("Oops! Something went wrong: $e");
+//     }
+//   }
+// }
 
 // API Keys
 
@@ -40,6 +43,3 @@ class ApiService {
 //     "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";
 
 // Key4 - uncomment this key and comment the rest to use this key
-
-const apikeyOpenReeveAi =
-    "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";
